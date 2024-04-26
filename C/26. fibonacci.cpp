ea609673cfab5c82c47f840b06f160f061a25658
DEEPAK KUMAR <116636205+deegetter03@users.noreply.github.com>
2024-04-26 10:46:19 +0530

    26th april

---
+++ C/26. fibonacci.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	int a = 0, b = 1, n, i, c;
+	printf("enter the terms\n");
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++)
+	{
+		printf("%d ", a);
+		c = a + b;
+		a = b;
+		b = c;
+	}
+	return 0;
+}
