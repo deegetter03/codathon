87b91e486706cc0a252fb885b3db8fbb92bc9284
DEEPAK KUMAR <116636205+deegetter03@users.noreply.github.com>
2024-05-04 08:30:52 +0530

    4th may

---
+++ C/34.pattern_print01.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	int n, i, j;
+	printf("enter the height of pattern\n");
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 1; j <= n; j++)
+		{
+			printf("%d\t", j);
+		}
+		printf("\n");
+	}
+	return 0;
+}
